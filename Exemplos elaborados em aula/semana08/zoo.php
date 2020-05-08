f47b382e3ec4052402e31deb384296458de8ccd0
Leonardo Villani <leonardo.villani@fatec.sp.gov.br>
2020-05-08 17:13:06 -0300

    Exemplos elaborados na Semana 08

--- Exemplos elaborados em aula/semana08/zoo.php
+++ Exemplos elaborados em aula/semana08/zoo.php
@@ -1,7 +1,24 @@
 <?php
 
-require 'Animal.php';
+// Arquivo que faz automaticamente o 'require' de arquivos necessários.
+require 'autoloader.php';
 
-// USANDO AS PSRs - PHP Standard Recommendation
+/**
+ * Imprime o som produzido pela boca de animais.
+ */
+function iniciar (AnimalInterface $obj)
+{
+    echo $obj->toTalk();
+}
 
-$obj = New Animal;
+// Instancia um objeto do tipo 'Dog' e armazena na variável $obj a referência para ele.
+$obj = new Dog;
+
+// Chamada da função 'iniciar'.
+iniciar($obj);
+
+// chamada da função 'iniciar' com um parâmetro de tipo incorreto.
+//iniciar(new Tomato); Tomato não fala.
+
+// Chamada da função 'iniciar' com outro parâmetro do tipo correto.
+iniciar(new Cat);
